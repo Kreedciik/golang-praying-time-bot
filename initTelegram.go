54219b324b0de8aac59227aba6cf05792e516b0b
Ilfat Islamgaliev <ilfatislamgaliev@gmail.com>
2024-11-08 00:09:46 +0100

    Initialized project, added telego package, rendered 2 buttons

---
+++ initTelegram.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+	"os"
+)
+
+const (
+	botToken = "7738528744:AAF0XrlX7czGkPZwgzJQ4uXubFOCb9GwyT8"
+)
+
+func initTelegramBot() {
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	updates, _ := bot.UpdatesViaLongPolling(nil)
+	defer bot.StopLongPolling()
+
+	// Create a keyboard
+	keyboard := tu.Keyboard(
+		tu.KeyboardRow(
+			tu.KeyboardButton("Get praying time by location").WithRequestLocation(),
+			tu.KeyboardButton("Get praying time by city"),
+		),
+	).WithResizeKeyboard().WithInputFieldPlaceholder("Select something...")
+
+	// Create a bot handler
+	bh, _ := th.NewBotHandler(bot, updates)
+	defer bh.Stop()
+
+	// Handle /start command
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		_, _ = bot.SendMessage(tu.Message(tu.ID(update.Message.Chat.ID), fmt.Sprintf("Hello %s! Welcome to this bot!", update.Message.From.FirstName)).WithReplyMarkup(keyboard))
+	}, th.CommandEqual("start"))
+
+	// Handle getting location
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		_, _ = bot.SendMessage(tu.Message(tu.ID(update.Message.Chat.ID), "Send me your location").WithReplyMarkup(keyboard))
+	}, th.TextEqual("Get praying time by location"))
+
+	// Handle getting city
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		_, _ = bot.SendMessage(tu.Message(tu.ID(update.Message.Chat.ID), "Send me your city").WithReplyMarkup(keyboard))
+	}, th.TextEqual("Get praying time by city"))
+
+	// Start handling updates
+	bh.Start()
+}
